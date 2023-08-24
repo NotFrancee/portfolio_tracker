d1782c7550b1917933dad391ba5118c2913ea91b
Andrea Franceschini <andrea.franceschini03@gmail.com>
2023-08-24 14:27:55 +0200

    added notes, amount, open_amount to to_row columns

--- classes/trade.py
+++ classes/trade.py
@@ -1,5 +1,4 @@
 from exceptions import MissingRequiredParams
-import pandas as pd
 
 
 class Trade:
@@ -15,6 +14,7 @@ class Trade:
         "amount",
         "price",
         "transaction_costs",
+        "notes",
     }
 
     to_row_columns = {
@@ -23,12 +23,14 @@ class Trade:
         "exchange": "Exchange",
         "broker": "Broker",
         "action": "Action",
-        "amount": "Amount",
         "currency": "Currency",
+        "amount": "Amount",
         "price": "Price",
         "transaction_costs": "Transaction Costs",
         "cost_basis": "Cost Basis",
         "unit_cost_basis": "Unit Cost Basis",
+        "open_amount": "Open Amount",
+        "notes": "Notes",
     }
 
     def __init__(self, trade_data: dict[str, str]):
@@ -41,9 +43,11 @@ class Trade:
         self.broker = trade_data["broker"]
         self.action = trade_data["action"].lower()
         self.amount = float(trade_data["amount"])
+        self.open_amount = self.amount
         self.currency = trade_data["currency"]
         self.price = float(trade_data["price"])
         self.transaction_costs = float(trade_data["transaction_costs"])
+        self.notes = trade_data["notes"]
 
         self.trade_data = trade_data
 
@@ -71,15 +75,6 @@ class Trade:
         return "\n".join(res)
 
     def to_row(self) -> list:
-        res = [getattr(self, key) for key in self.to_row_columns]
+        res = [str(getattr(self, key)) for key in self.to_row_columns]
 
         return res
-
-    @staticmethod
-    def trades_to_df(trades: list) -> pd.DataFrame:
-        trades_rows = [trade.to_row() for trade in trades]
-        trades_df = pd.DataFrame(trades_rows, columns=Trade.to_row_columns)
-        trades_df = trades_df.set_index("date")
-        trades_df.loc[:, "open_amount"] = trades_df["amount"]
-
-        return trades_df
