15783354738fce727985ad58919a1134b6fab379
Andrea Franceschini <andrea.franceschini03@gmail.com>
2023-08-30 09:23:34 +0200

    removed old trade_builder

--- util/trade_builder.py
+++
@@ -1,9 +0,0 @@
-class TradeBuilder:
-    def long_short(self, long_tickers, short_tickers, benchmark):
-        def convert_to_list(tickers):
-            if long_tickers is list:
-                pass
-            else:
-                pass
-
-        convert_to_list(long_tickers)
