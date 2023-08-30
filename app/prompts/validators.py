f487651d91902fec36e5e317c44489e6a0a9f372
Andrea Franceschini <andrea.franceschini03@gmail.com>
2023-08-30 14:19:52 +0200

    comments + base class declaration

--- app/prompts/validators.py
+++ app/prompts/validators.py
@@ -4,25 +4,30 @@ from abc import ABCMeta, abstractmethod
 
 @six.add_metaclass(ABCMeta)
 class Validator:
-    """Base class for the validator. Override validate and process methods, which will later be used by the Prompt Class"""
+    """Base class for the validator.
+    Override validate and process methods, which will later be used by the Prompt Class
+    """
 
     def __init__(self) -> None:
         pass
 
     @abstractmethod
-    def validate(self, input: str):
-        """You must override this function. Return false is not valid, True if valid"""
-        pass
+    def validate(self, input_str: str):
+        """You must override this function.
+        Return false is not valid, True if valid
+        """
+
+        return True
 
     @abstractmethod
     def process(self, input_str: str):
-        pass
+        return input_str
 
 
 class DateValidator(Validator):
-    def validate(self, input: str):
-        print("wip")
-        return super().validate(input)
+    """Validate dates"""
+
+    def validate(self, input_str: str):
 
     def process(self, input_str: str):
         print("wip")
